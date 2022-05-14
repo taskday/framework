f21f05e6d8d6acf93cefafa6a8cb3b026ee6f36c
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2022-05-14 10:51:49 +0200

    Add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// prettier.config.js
+module.exports = {
+  tailwindConfig: './tailwind.config.js',
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  arrowParens: "always",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  printWidth: 120,
+}
