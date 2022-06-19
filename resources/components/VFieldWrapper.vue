c6e1cb2bd564c1b9492f2822d249f37ac8c2edb4
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2022-06-19 12:25:29 +0200

    wip

--- resources/components/VFieldWrapper.vue
+++ resources/components/VFieldWrapper.vue
@@ -5,6 +5,7 @@
     :field="field"
     :key="field.pivot?.value"
     :card="card"
+    class="h-8 flex items-center justify-center"
   />
 </template>
 
--- resources/components/VFormInput.vue
+++ resources/components/VFormInput.vue
@@ -2,7 +2,7 @@
   <label class="block w-full">
     <span v-if="label" class="block mb-1 text-gray-700 dark:text-gray-300">{{ label }}</span>
     <input
-      class="w-full h-9 px-2 placeholder-gray-300 transition border border-gray-200 rounded dark:border-gray-800 focus:outline-none focus:border-transparent focus:ring-1 focus:ring-offset-blue-400 dark:focus:ring-offset-blue-700 focus:shadow-sm dark:ring-opacity-10 dark:bg-gray-700 dark:text-gray-100"
+      class="w-full h-9 px-2 placeholder-gray-300 transition border border-gray-200 rounded-md dark:border-gray-800 focus:outline-none focus:border-transparent focus:ring-1 focus:ring-offset-blue-400 dark:focus:ring-offset-blue-700 focus:shadow-sm dark:ring-opacity-10 dark:bg-gray-700 dark:text-gray-100"
       v-bind="$attrs"
       :value="modelValue"
       @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" />
